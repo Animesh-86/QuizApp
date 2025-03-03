4a97f0f6172f5035f9487569e09c1f99d1350947
Animesh <animeshiit0806@gmail.com>
2025-03-03 16:01:21 +0530

    Added new Screen

---
+++ adv_basics/lib/question_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _QuestionScreenState();
+  }
+}
+
+class _QuestionScreenState extends State<QuestionScreen>{
+  @override
+  Widget build(context){
+    return const Text("Hello, World!");
+  }
+}
