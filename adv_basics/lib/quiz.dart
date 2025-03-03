738241148ff42b69d3a3d96c048ca8129efea8c2
Animesh <animeshiit0806@gmail.com>
2025-03-03 15:39:48 +0530

    Initial commit

---
+++ adv_basics/lib/quiz.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 156, 76, 248),
+                Color.fromARGB(255, 68, 11, 92),
+              ],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
---
+++ adv_basics/lib/start_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(131, 255, 255, 255),
+          ),
+          /*Opacity(
+            opacity: 0.8,
+            child: Image.asset('assets/images/quiz-logo.png', width: 300),
+          ), */
+          const SizedBox(height: 80),
+          const Text(
+            "Learn Flutter",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: Icon(Icons.arrow_right_alt),
+            label: Text("Start Quiz"),
+          ),
+        ],
+      ),
+    );
+  }
+}
